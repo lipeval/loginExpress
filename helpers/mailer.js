3bf12bb9871ef5fc5a1ef9a072ce8ccbd0868e06
Felipe Vallina <fvallinalp@gmail.com>
2018-07-12 12:09:06 -0500

    templates

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -1,5 +1,6 @@
 const nodemailer = require('nodemailer');
-
+const hbs = require('hbs');
+const fs = require('fs')
 const transporter = nodemailer.createTransport({
     
     service: 'Gmail',
@@ -22,4 +23,19 @@ exports.sendWelcomeMail = (user)=>{
     .then(info=> console.log(info))
     .catch(error=> console.log(error))
 
+}
+
+const welcomeCompile = hbs.compile(fs.readFileSync((__dirname, './views/welcome.hbs'), 'utf8'));
+
+exports.sendTemplate = ()=>{
+    const data = {
+        from: 'Holaaa 👻" <myawesome@project.com>',
+        to: user.email, 
+        subject: 'Prueba',
+        //text: `Hola ${user.username} Bienvenido a nuestra APP`
+        html: welcomeCompile(user)
+    }
+    transporter.sendMail(data)
+    .then(info=> console.log(info))
+    .catch(error=> console.log(error))
 }
